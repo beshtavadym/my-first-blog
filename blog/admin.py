6fc933ca7d3f67270e216d47c00f4c107d413618
Beshta Vadym <morngen11@gmail.com>
2021-10-01 20:29:21 +0300

    My Django app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.db import models
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from django.utils.translation import gettext_lazy as _
+from ckeditor.widgets import CKEditorWidget
+
+from .models import Post
+admin.site.register(Post)
+
+
+class FlatPageAdmin(FlatPageAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
